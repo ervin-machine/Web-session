03950fdd48d0a86ed210efe99a7d50a26045e54c
unknown <ervinhodzic1234@gmail.com>
2021-10-14 16:01:00 +0200

    web-session

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { useState } from 'react'
+import './App.css'
 import { createBrowserHistory } from 'history';
-
-import ReactWebSession, { updateSession } from 'react-web-session';
-
+import ReactWebSession from 'react-web-session';
 const history = createBrowserHistory();
 function App() {
-  const [data, setData] = useState([]);
-  
+  const [createdDatas, setCreatedData] = useState([]);
+  const [expiresDatas, setExpiresData] = useState([]);
   const HandleCallBack = (data) => {
-    setData(data);
-    console.log(data)
+    setCreatedData(data.origin);
+    setExpiresData(data.current);
   }
   return (
     <div>
-      <h2>Hello my friend, check console for your session</h2>
+      <h2 className="head-name">React Web Session</h2>
+      <p>Session created at: {createdDatas.createdAt}</p>
+      <p>Session expires at: {expiresDatas.expiresAt}</p>
       <ReactWebSession
       callback={HandleCallBack}
       data={{ newUser: true, purchases: [] }}
       history={history}
-      timezone="America/Sao_Paulo"
       />
       
     </div>
